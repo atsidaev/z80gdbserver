3e9cd78f97d059805b8e19af3481864adb9a37f3
Alexandr Tsidaev <a.tsidaev@gmail.com>
2011-09-25 18:35:23 +0600

    Startup info message added Signed-off-by: Alexandr Tsidaev <a.tsidaev@gmail.com>

--- z80gdbserver/Main.cs
+++ z80gdbserver/Main.cs
@@ -16,11 +16,18 @@ namespace z80gdbserver
 			byte[] file = null;
 			if (args.Count() > 0)
 				file = File.ReadAllBytes(args[0]);
+			else
+			{
+				Console.WriteLine("Usage: z80gdbserver [--pause] [filename]");
+				Console.WriteLine("Server is using 2000 TCP port");
+				Console.WriteLine("filename not specified so using hardcoded example");
+				Console.WriteLine("-----");
+			}
 			
 			IEmulator emulator = new TestEmulator(file);
 			IDebugBridge gdb = new GNUDebugBridge();
 			gdb.Initialize(emulator);
-			if (!args.Any(a => a == "/p"))
+			if (!args.Any(a => a == "--pause"))
 				emulator.Run();
 			
 			// Infinite loop
