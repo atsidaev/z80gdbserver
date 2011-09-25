42cb897f52a24ffce58a55136bb291b7365d899f
Alexandr Tsidaev <a.tsidaev@gmail.com>
2011-09-25 19:05:25 +0600

    Licence information added

--- z80gdbserver/Properties/AssemblyInfo.cs
+++ z80gdbserver/Properties/AssemblyInfo.cs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Alexander Tsidaev
+ * 
+ * This file is part of z80gdbserver.
+ * z80gdbserver is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * z80gdbserver is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with z80gdbserver. 
+ * If not, see http://www.gnu.org/licenses/.
+ */
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -5,11 +21,11 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("z80gdbserver")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GNU debug bridge server for z80 processor emulators")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Alexander Tsidaev")]
+[assembly: AssemblyProduct("z80gdbserver")]
+[assembly: AssemblyCopyright("Alexander Tsidaev, 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
